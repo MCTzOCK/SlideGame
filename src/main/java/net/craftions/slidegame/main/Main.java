dc28b7092045ee81362927ea046a2d46543a77a6
Ben Siebert <dev@mctzock.de>
2021-05-25 17:35:32 +0200

    lol

--- src/main/java/net/craftions/slidegame/main/Main.java
+++ src/main/java/net/craftions/slidegame/main/Main.java
@@ -3,22 +3,11 @@
  */
 package net.craftions.slidegame.main;
 
-import net.craftions.slidegame.block.Block;
-import net.craftions.slidegame.block.BlockBuilder;
-import net.craftions.slidegame.block.Blocks;
-
-import java.awt.*;
+import net.craftions.slidegame.ui.GameUI;
 
 public class Main {
 
     public static void main(String[] args) {
-        Block block = new BlockBuilder().setValue(1).setColor(Color.BLACK).build();
-        Block block2 = new BlockBuilder().setValue(1).setColor(Color.BLUE).build();
-        if(Blocks.canMergeBlocks(block, block2)){
-            Block fBlock = Blocks.mergeBlocks(block, block2);
-            System.out.println("V: " + fBlock.getValue());
-        }else {
-            System.out.println("Could not merge!");
-        }
+        GameUI.createUI();
     }
 }
---
+++ src/main/java/net/craftions/slidegame/ui/GameUI.java
@@ -0,0 +1,39 @@
+package net.craftions.slidegame.ui;
+
+import net.craftions.slidegame.block.Block;
+import net.craftions.slidegame.block.BlockBuilder;
+import net.craftions.slidegame.block.Blocks;
+
+import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class GameUI {
+    public static JFrame jf = new JFrame("SlideGame");
+    public static JLabel field = new JLabel();
+    public static Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+    public static ArrayList<Block> blockArr = new ArrayList<>();
+
+    public static void createUI() {
+        jf.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        jf.setLayout(null);
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+
+        field.setLayout(null);
+        field.setIcon(new ImageIcon("/Users/ben/Desktop/fuck.png"));
+        field.setBounds((dim.width / 3), 0, (dim.width / 3), dim.height);
+
+        Block block = new BlockBuilder()
+                .setValue(10)
+                .setColor(Color.RED)
+                .build();
+
+        jf.add(field);
+    }
+
+    public static void moveBlocks(){
+        blockArr
+    }
+}
